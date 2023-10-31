5d46876205edeb27abae8118a5e41a6f098b1cc4
guimbly <pietropaiva.pp@hotmail.com>
2023-10-31 12:17:48 -0300

    commit final, tratados erros no range 5000~6000 e adicionado else pra todas as outras situações

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 let heroName = 'Guimbly'
-let heroExp = 10
-switch(heroExp){
-    case heroExp <= 1000:
-        console.log("O Herói " + heroName + 'está no nivel Ferro!')
-    break
+let heroExp = 9000
+let msgStart = "O Herói "
+let msgEnd = ' está no nivel '
 
-    case heroExp > 1000 && heroExp <= 2000:
-        console.log("O Herói " + heroName + 'está no nivel Bronze!')
-    break
+if(heroExp <= 1000){
+    console.log(msgStart + heroName + msgEnd +'Ferro!')
+    }
 
-    case heroExp > 2000 && heroExp <= 5000:
-        console.log("O Herói " + heroName + 'está no nivel Prata!')
-    break
+else if(heroExp > 1000 && heroExp <= 2000){
+    console.log(msgStart + heroName + msgEnd +'Bronze!')
+    }
 
-    case heroExp > 6000 && heroExp <= 7000:
-        console.log("O Herói " + heroName + 'está no nivel Ouro!')
-    break
+else if(heroExp > 2000 && heroExp <= 5000){
+    console.log(msgStart + heroName + msgEnd +'Prata!')
+    }
 
-    case heroExp > 7000 && heroExp <= 8000:
-        console.log("O Herói " + heroName + 'está no nivel Platina!')
-    break
+else if(heroExp > 5000 && heroExp <= 6000){
+        console.log('Range não especificado no exercicio, 5000 ~ 6000. Talvez seja um ranking secreto.')
+        }
 
-    case heroExp > 8000 && heroExp <= 9000:
-        console.log("O Herói " + heroName + 'está no nivel Ascendente!')
-    break
+else if(heroExp > 6000 && heroExp <= 7000){
+    console.log(msgStart + heroName + msgEnd +'Ouro!')
+    }
 
-    case heroExp > 9000 && heroExp <= 10000:
-        console.log("O Herói " + heroName + 'está no nivel Imortal!')
-    break
+else if(heroExp > 7000 && heroExp <= 8000){
+    console.log(msgStart + heroName + msgEnd +'Platina!')
+    }
 
-    case heroExp > 10000:
-        console.log("O Herói " + heroName + 'está no nivel Radiante!')
-    break
-}
+else if(heroExp > 8000 && heroExp <= 9000){
+    console.log(msgStart + heroName + msgEnd +'Ascendente!')
+    }
+
+else if(heroExp > 9000 && heroExp <= 10000){
+    console.log(msgStart + heroName + msgEnd +'Imortal!')
+    }
+
+else if(heroExp > 10000){
+        console.log(msgStart + heroName + msgEnd +'Radiante!')
+        }
+else{
+    console.log('Temos um problema em descobrir seu ranking... Você é pro?')
+    }
